4f53f2313aaf5baff55bcae242adadda1167c7e9
Sydur Rahman <sydurrahmant1@gmail.com>
2026-08-22 16:18:55 +0600

    Update Changes

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,8 +40,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => rtrim(env('APP_URL', 'http://localhost'), '/').'/'.env('STORAGE_SYMLINK_NAME', 'storage'),
+            'root' => base_path('storage'),
+            'url' => rtrim(env('APP_URL', 'http://localhost'), '/').'/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
